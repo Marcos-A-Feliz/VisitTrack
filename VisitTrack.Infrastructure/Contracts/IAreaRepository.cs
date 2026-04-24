3c4ae0ae5cd65365d72017b6340b17aab50b3ca4
Marcos-A-Feliz <marcosadrianchenfeliz@gmail.com>
2026-04-24 05:48:06 -0400

    Correcion de errores infraestructura y sln

---
+++ VisitTrack.Infrastructure/Contracts/IAreaRepository.cs
@@ -0,0 +1,9 @@
+﻿
+using VisitTrack.Domain.Entities;
+
+namespace VisitTrack.Infrastructure.Contracts;
+
+public interface IAreaRepository : IRepository<Area>
+{
+    Task<Area?> GetByNombreAsync(string nombre);
+}
---
+++ VisitTrack.Infrastructure/Contracts/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿
+namespace VisitTrack.Infrastructure.Contracts;
+
+public interface IUnitOfWork : IDisposable
+{
+    IVisitanteRepository Visitantes { get; }
+    IAreaRepository Areas { get; }
+    IEmpleadoRepository Empleados { get; }
+    IVisitaRepository Visitas { get; }
+    IUserRepository Users { get; }
+
+    Task<int> SaveChangesAsync();
+}
