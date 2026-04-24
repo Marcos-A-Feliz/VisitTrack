e2b5dc41ee7a7bbc5e67fe78166ef4c94cd68c44
Marcos-A-Feliz <marcosadrianchenfeliz@gmail.com>
2026-04-24 04:27:41 -0400

    Creando Entidades y actualizaciones varias

---
+++ VisitTrack.Repositories/Entities/Visita.cs
@@ -0,0 +1,21 @@
+﻿namespace VisitTrack.Domain.Entities;
+
+public class Visita
+{
+    public int Id { get; set; }
+    public DateTime FechaEntrada { get; set; }
+    public DateTime? FechaSalida { get; set; }
+    public required string Motivo { get; set; }
+    public string Estado { get; set; } = "Pendiente";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public int VisitanteId { get; set; }
+    public int EmpleadoResponsableId { get; set; }
+    public int AreaId { get; set; }
+    public int UserId { get; set; }
+
+    public Visitante? Visitante { get; set; }
+    public Empleado? EmpleadoResponsable { get; set; }
+    public Area? Area { get; set; }
+    public User? User { get; set; }
+}
